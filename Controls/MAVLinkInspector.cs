784aa3a291b36d33d93ce1be650bf6b2ba9a9788
Tatsuya Yamaguchi <susumu-336@hotmail.co.jp>
2020-12-24 16:44:42 +0900

    MAVLinkInspector: fix the unit of bytes per second

--- Controls/MAVLinkInspector.cs
+++ Controls/MAVLinkInspector.cs
@@ -109,6 +109,7 @@ namespace MissionPlanner.Controls
                                   (mavi.SeenRate(mavLinkMessage.sysid, mavLinkMessage.compid, mavLinkMessage.msgid))
                                   .ToString("0.0 Hz") + ", #" + mavLinkMessage.msgid + ") " +
                                   mavi.SeenBps(mavLinkMessage.sysid, mavLinkMessage.compid, mavLinkMessage.msgid).ToString("0bps");
+                                  mavi.SeenBps(mavLinkMessage.sysid, mavLinkMessage.compid, mavLinkMessage.msgid).ToString("0Bps");
 
                 if (msgidnode.Text != msgidheader)
                     msgidnode.Text = msgidheader;
